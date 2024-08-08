3a386dc59751890b7f6e87219caade80e1d9943d
PrakashDhangi <prakashdhangi6@gmail.com>
2024-08-08 08:31:04 +0530

    this is my first program

---
+++ Hello/src/Main.java
@@ -0,0 +1,15 @@
+package Hello.src;
+
+public class Main {
+
+    public static void main(String[] arguments) {
+        System.out.println("Hello World");
+        User ur=new User();
+        //User ur1=new User();
+        ur.age=20;
+        ur.Name="Prakash";
+        ur.Gender="Male";
+        System.out.println(ur.age+ "  "+ur.Name+"  "+ur.Gender);
+        ur.Walk();
+    }
+    }
---
+++ Hello/src/User.java
@@ -0,0 +1,13 @@
+package Hello.src;
+
+public class User
+{
+    int age;
+    String Name;
+    String Gender;
+
+    public void Walk()
+    {
+        System.out.println("Walking");
+    }
+}
